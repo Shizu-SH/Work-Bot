aac17d73214425d78c30c50cada22c9667aac128
Shizu-SH <119007576+Shizu-SH@users.noreply.github.com>
2022-12-10 13:01:12 +0000

    Update ejemplo-utilizacion.js

--- plugins/ejemplo-utilizacion.js
+++ plugins/ejemplo-utilizacion.js
@@ -32,8 +32,8 @@ POR EJEMPLO COMPRARAS UNAS ZAPATILLAS
 PULSA START PARA EMPEZAR CON LA COMPRA DE PRUEBA
 `.trim()
 conn.sendButton(m.chat, str.trim(), wm, null, [
-['START', `#donar`],
-['VOLVER AL MENU', `#owner`]
+['START', `#start`],
+['VOLVER AL MENU', `#menu`]
 ], m, { mentions: [m.sender] })
 } catch {
 conn.reply(m.chat, '[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃', m)
